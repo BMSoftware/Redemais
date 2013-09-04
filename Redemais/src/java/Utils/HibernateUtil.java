a1e2650579c3752d0af93ae25b6faf9bedbcf27f
Bruno Alves dos Santos <Bruno@BrunoPC>
2013-09-04 00:50:52 -0300

    Criado o HibernateUtil

---
+++ Redemais/src/java/Utils/HibernateUtil.java
@@ -0,0 +1,46 @@
+package Utils;
+
+import model.Bancos;
+import model.Cancelamento;
+import model.Cliente;
+import model.Empresa;
+import model.Usuario;
+import model.Venda;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author Bruno
+ */
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                AnnotationConfiguration ac = new AnnotationConfiguration();
+                ac.addAnnotatedClass(Bancos.class);
+                ac.addAnnotatedClass(Cancelamento.class);
+                ac.addAnnotatedClass(Cliente.class);
+                ac.addAnnotatedClass(Empresa.class);
+                ac.addAnnotatedClass(Usuario.class);
+                ac.addAnnotatedClass(Venda.class);
+                sessionFactory = ac.configure().buildSessionFactory();
+            } catch (Throwable ex) {
+                // Log the exception.
+                System.err.println("Initial SessionFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+            return sessionFactory;
+        } else {
+            return sessionFactory;
+        }
+    }
+}
